499cff00917bc0d3818fc6dd040fbab17bae8fc9
Betterjingxuan <50142446+Betterjingxuan@users.noreply.github.com>
2024-10-24 13:58:39 +0800

    Update ShapMatrixEntry.java

--- src/main/java/structure/ShapMatrixEntry.java
+++ src/main/java/structure/ShapMatrixEntry.java
@@ -5,5 +5,5 @@ import java.util.ArrayList;
 public class ShapMatrixEntry {
     public double sum;
     public int count;
-    public ArrayList<Double> record = new ArrayList<>();  //记录计算结果，计算方差
+    public ArrayList<Double> record = new ArrayList<>(); 
 }
